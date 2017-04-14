89fbe0653690e48dc6efaacb144a46e8d54ae27e
Tran Van Tin <nit@Trans-Mac.local>
2017-04-14 10:04:40 +0700

    Initial Commit

---
+++ calculator_practice1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator_practice1
+//
+//  Created by Tran Van Tin on 4/14/17.
+//  Copyright © 2017 Tran Van Tin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
